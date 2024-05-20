ea4d66f7ded30e0420c289a636f37cfc7cd8f7ba
liamrl <liamr24@nycstudents.net>
2024-05-20 09:32:11 -0400

    5/20/24

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Board {
+public class Board {
+
+    private int[][] grid;
+
+    public Board(){
+        grid = new int[4][4];
+    }
+
+    public void nextNum(){
+        int count = 0;
+        for (int[] arr : grid){
+            for (int i : arr){
+                if (i == 0){
+                    count++;
+                }
+            }
+        }
+
+        int nextPlace = (int) (Math.random() * count);
+    }
 }
