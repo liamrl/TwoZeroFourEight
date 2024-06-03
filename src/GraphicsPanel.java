485598baf3ea259eb5678ea5c19108c90d41b9f5
liamrl <liamrenaudlevine@gmail.com>
2024-06-03 09:22:30 -0400

    6/3/24

--- src/GraphicsPanel.java
+++ src/GraphicsPanel.java
@@ -11,24 +11,110 @@ public class GraphicsPanel extends JPanel {
     public GraphicsPanel() {
     }
 
+    Board b = new Board();
+
+
+    private Color changeColor(int x, int y){
+        switch (b.getPosition(0, 0)) {
+            case 2 ->{
+                return Color.LIGHT_GRAY;
+            }
+            case 4 ->{
+                return Color.GRAY;
+            }
+            case 8 -> {
+                return Color.YELLOW;
+            }
+            case 16 -> {
+                return Color.ORANGE;
+            }
+            case 32 -> {
+                return Color.PINK;
+            }
+            case 64 -> {
+                return Color.MAGENTA;
+            }
+            case 128 ->{
+                return Color.RED;
+            }
+            case 256 ->{
+                return Color.GREEN;
+            }
+            case 512 ->{
+                return Color.CYAN;
+            }
+            case 1024 ->{
+                return Color.BLUE;
+            }
+            case 2048 ->{
+                return Color.BLACK;
+            }
+
+        }
+        return Color.WHITE;
+    }
+
     @Override
     public void paintComponent(Graphics g) {
-        Board b = new Board();
         super.paintComponent(g);  // just do this
-        if (b.getPosition(0,0) > 0)
-        g.setColor(Color.YELLOW);
+        g.setColor(changeColor(0,0));
         g.fillRect(5, 5, 100, 100);
-        g.setColor(Color.YELLOW);
+
+        g.setColor(changeColor(0,1));
         g.fillRect(5,110 , 100, 100);
-        g.setColor(Color.YELLOW);
+
+        g.setColor(changeColor(0,2));
         g.fillRect(5, 215 , 100, 100);
-        g.setColor(Color.YELLOW);
+
+        g.setColor(changeColor(0,3));
         g.fillRect(5,320, 100, 100);
-        g.setColor(Color.YELLOW);
+
+        g.setColor(changeColor(1,0));
         g.fillRect(110, 5, 100, 100);
-        g.setColor(Color.YELLOW);
+
+        g.setColor(changeColor(1,1));
         g.fillRect(110,110 , 100, 100);
 
+        g.setColor(changeColor(1,2));
+        g.fillRect(110, 215, 100, 100);
+
+        g.setColor(changeColor(1,3));
+        g.fillRect(110,320 , 100, 100);
+
+        g.setColor(changeColor(2,0));
+        g.fillRect(215, 5, 100, 100);
+
+        g.setColor(changeColor(2,1));
+        g.fillRect(215,110 , 100, 100);
+
+        g.setColor(changeColor(2,2));
+        g.fillRect(215, 215, 100, 100);
+
+        g.setColor(changeColor(2,3));
+        g.fillRect(215,320 , 100, 100);
+
+        g.setColor(changeColor(3,0));
+        g.fillRect(320, 5, 100, 100);
+
+        g.setColor(changeColor(3,1));
+        g.fillRect(320,110 , 100, 100);
+
+        g.setColor(changeColor(3,2));
+        g.fillRect(320, 215, 100, 100);
+
+        g.setColor(changeColor(3,3));
+        g.fillRect(320,320 , 100, 100);
+
+    }
+
+    public void Play(){
+
+            b.nextNumAndPlace();
+            b.moveUp();
+
+            b.nextNumAndPlace();
+            b.moveDown();
+            b.nextNumAndPlace();
 
     }
 }
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -7,12 +8,14 @@ public class MainFrame implements Runnable, KeyListener {
 
     public MainFrame() {
         JFrame frame = new JFrame("2048");
+        frame.setBackground(Color.WHITE);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(430, 430);
+        frame.setSize(440, 465);
         frame.setLocation(300, 50);
         // create and add panel
         panel = new GraphicsPanel();
         frame.add(panel);
+        panel.Play();
 
         // display the frame
         frame.setVisible(true);
@@ -21,6 +24,7 @@ public class MainFrame implements Runnable, KeyListener {
         Thread thread = new Thread(this);
         thread.start();
     }
+
 
 
 
